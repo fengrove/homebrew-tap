ed36a8dac2efd3392a41b5d76e5400bf4c792f80
Peter Bjorklund <piot@hotmail.com>
2025-03-11 16:48:57 +0100

    feat: extract packages files to `pkgshare`

--- Formula/mangrove.rb
+++ Formula/mangrove.rb
@@ -5,28 +5,41 @@ class Mangrove < Formula
   version "0.0.15"
   sha256 "6fcf12cfafaa84657fb9485728c239c60d9965ca515eecfb1c8be58c31659421"
 
+  resource "packages" do
+    url "https://github.com/swamp/mangrove/releases/download/v0.0.15/packages.tar.gz"
+    sha256 "d6e2d55264bf5d3873a4c918a84071308d9ca854ca1ee1f4922f1eebee27eae9"
+  end
+
+
   def install
     prefix.install Dir["mangrove/*"]
     system "xattr", "-cr", prefix
     bin.install "mangrove" => "mangrove"
 
-    # --- Install Default User Config File ---
-    # config_dir = ~/.config/"mangrove"
-    #FileUtils.mkdir_p config_dir
-    #default_config_source = prefix/"config"/"default.config" # Assuming default.config is in 'config' dir in archive
-    #user_config_dest = config_dir/"config.toml"
-
-    #unless File.exist?(user_config_dest) # Only copy if user config doesn't exist
-    #  FileUtils.cp default_config_source, user_config_dest
-    #  puts "Created default config file at: #{user_config_dest}"
-    #end
-
+    resource("packages").stage do
+      pkgshare.install Dir["*"]
+    end
   end
 
   def caveats
     s = <<~EOS
       mangrove has been installed to:
         #{prefix}
+
+      Mangrove has installed its additional packages into:
+        #{pkgshare}/packages
+
+      By default, Mangrove expects its packages to be located in ~/.swamp/packages.
+      To ensure Mangrove can find these files, you have two options:
+
+        1. Update the SWAMP_HOME environment variable:
+           export SWAMP_HOME="#{pkgshare}"
+
+        2. Copy the packages to your home directory:
+           mkdir -p ~/.swamp/packages
+           cp -R #{pkgshare}/packages/* ~/.swamp/packages/
+
+      Choose the option that works best for your workflow.
 
       Executable is available as: `mangrove`
     EOS
