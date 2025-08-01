6c507a963a61cc64c928bdc75d20e992d3ed0a73
Peter Bjorklund <piot@hotmail.com>
2025-08-01 14:34:07 +0200

    chore: update to `0.1.30`

--- Formula/mangrove.rb
+++ Formula/mangrove.rb
@@ -2,13 +2,13 @@ class Mangrove < Formula
   desc "Mangrove (Unsigned)"
   homepage "https://github.com/fengrove"
 
-	MG_VERSION = "0.1.28" # brew is annoying that you can't use the normal version field
+	MG_VERSION = "0.1.30" # brew is annoying that you can't use the normal version field
 	version MG_VERSION
 	# revision 2 # only need revision number if a previous git push was incorrect
 
 	on_arm do
 		url "https://github.com/fengrove/fengrove/releases/download/v#{MG_VERSION}/mangrove-darwin-arm64.tar.gz"
-		sha256 "167ed66f8ef338785eafee96a5d64dac40f4292083a032e2bd298fe230a89708"
+		sha256 "e221369736e8901f50cc78ee3127e992a9b0e1a8e5e43df50d2e38f2ecff8629"
 	end
 
 	#on_intel do
@@ -19,7 +19,7 @@ class Mangrove < Formula
 	# Single resource for all architectures
 	resource "packages" do
 		url "https://github.com/fengrove/fengrove/releases/download/v#{MG_VERSION}/packages.tar.gz"
-		sha256 "465898edcea33a899040d3b426feadde38f90c5104a16ecf8708367cb3ff59e4"
+		sha256 "2c8b373ff201fb540c4a510eaf6f0c0f9c5367d33451f1b14110a5d7ea724ac0"
 	end
 
 
