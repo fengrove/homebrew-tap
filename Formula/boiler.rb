361ec9cff1822a5768a98b984ef763ffd23e95ef
Peter Bjorklund <piot@hotmail.com>
2025-09-13 11:35:47 +0200

    chore: boiler `0.0.4`

--- Formula/boiler.rb
+++ Formula/boiler.rb
@@ -2,14 +2,14 @@ class Boiler < Formula
   desc "Boiler (Unsigned)"
   homepage "https://github.com/piot/boiler"
 
-	MG_VERSION = "0.0.3" # brew is annoying that you can't use the normal version field
+	MG_VERSION = "0.0.4" # brew is annoying that you can't use the normal version field
 	version MG_VERSION
 	# revision 2 # only need revision number if a previous git push was incorrect
 
 	on_macos do
 		on_arm do
 			url "https://github.com/piot/boiler/releases/download/v#{MG_VERSION}/boiler-darwin-arm64.tar.gz"
-			sha256 "7abf7896a732536a1c5a789262979e331b98f97c3be5e0bfb43fe03d28928058"
+			sha256 "8ec34c413e7bdb46a71917fd14b2f7ab85b573922c59c16b090d9de32d0fceb9"
 		end
 	end
 
